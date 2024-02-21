6b53997eb68834cea2e6e7abc0f188732ca67d51
baraganio <carlossrsi07@gmail.com>
2024-02-21 20:15:12 +0100

    Added some buttons to the main page

--- webapp/src/components/Login.js
+++ webapp/src/components/Login.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Container, Typography, TextField, Button, Snackbar } from '@mui/material';
+import Game from './Game';
 
 const Login = () => {
   const [username, setUsername] = useState('');
@@ -10,6 +11,12 @@ const Login = () => {
   const [loginSuccess, setLoginSuccess] = useState(false);
   const [createdAt, setCreatedAt] = useState('');
   const [openSnackbar, setOpenSnackbar] = useState(false);
+
+  // Declara la variable showGame y el metodo que la modifica. Se inicializa a false
+  const [showGame, setShowGame] = useState(false);
+
+  // Declara la variable showHistoricalData y el metodo que la modifica. Se inicializa a false
+  const [showHistoricalData, setShowHistoricaData] = useState(false);
 
   const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
 
@@ -33,17 +40,35 @@ const Login = () => {
     setOpenSnackbar(false);
   };
 
+  const handleShowGame = () => {
+    setShowGame(true);
+  };
+
+  const handleShowHistoricalData = () => {
+    setShowHistoricaData(true);
+  };
+
   return (
     <Container component="main" maxWidth="xs" sx={{ marginTop: 4 }}>
       {loginSuccess ? (
-        <div>
-          <Typography component="h1" variant="h5" sx={{ textAlign: 'center' }}>
-            Hello {username}!
-          </Typography>
-          <Typography component="p" variant="body1" sx={{ textAlign: 'center', marginTop: 2 }}>
-            Your account was created on {new Date(createdAt).toLocaleDateString()}.
-          </Typography>
-        </div>
+        showGame ? (
+          <Game/>
+        ) : (
+          <div>
+            <Typography component="h1" variant="h5" sx={{ textAlign: 'center' }}>
+              Hello {username}!
+            </Typography>
+            <Typography component="p" variant="body1" sx={{ textAlign: 'center', marginTop: 2 }}>
+              Your account was created on {new Date(createdAt).toLocaleDateString()}.
+            </Typography>
+            <Button variant="contained" color="primary" onClick={handleShowGame}>
+              Empieza juego
+            </Button>
+            <Button variant="contained" color="primary" onClick={handleShowHistoricalData}>
+              Historico de partidas de jugador
+            </Button>
+          </div>
+        )
       ) : (
         <div>
           <Typography component="h1" variant="h5">
