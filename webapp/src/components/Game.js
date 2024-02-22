65493f038c4e7e4c1d3ceee0e59efeeca5f466e9
baraganio <carlossrsi07@gmail.com>
2024-02-22 01:27:32 +0100

    Creation of questions creation service, and demo of questions generation with wikidata

--- webapp/src/components/Game.js
+++ webapp/src/components/Game.js
@@ -3,39 +3,69 @@ import axios from 'axios';
 import { Container, Typography, TextField, Button, Snackbar } from '@mui/material';
 
 const Game = () => {
-  /*const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const [loginSuccess, setLoginSuccess] = useState(false);
-  const [createdAt, setCreatedAt] = useState('');
-  const [openSnackbar, setOpenSnackbar] = useState(false);
+
+  const [askForQuestion, setAskForQuestion] = useState(false);
+  const [pais, setpais] = useState('');
+  const [capital, setcapital] = useState('');
 
   const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
+  
+  // Esta es la llamada al servicio de generar las preguntas
+  const  handleShowQuestion = async () => {
+    //setAskForQuestion(true);
+    try{
+      // Se declara esta variable unicamente para probar cosas con ella en la peticion
+      const eyou = "aa"
+      // Se hace una peticion a la api (llega a gateway-service.js) con la opcion createquestion
+      // y los parametros de entrada aa, aa
+      const response = await axios.post(`${apiEndpoint}/createquestion`, { eyou, eyou });
+      console.log(response);
+    }catch (error){
+      console.error('Error:', error);
+    }    
+  }
 
-  const loginUser = async () => {
-    try {
-      const response = await axios.post(`${apiEndpoint}/login`, { username, password });
-
-      // Extract data from the response
-      const { createdAt: userCreatedAt } = response.data;
-
-      setCreatedAt(userCreatedAt);
-      setLoginSuccess(true);
-
-      setOpenSnackbar(true);
-    } catch (error) {
-      setError(error.response.data.error);
+  // TODO ESTO ES LO QUE ESTA COMENTADO EN CREATION-SERVICE.JS
+  // CREO QUE DEBERIA IR ALLI PERO COMO NO FUNCIONA LO PROBE AQUI
+  const deberiaIrEnelServicio = async () => {
+    setAskForQuestion(true);
+    const sparqlQuery = 'SELECT DISTINCT ?country ?countryLabel ?capital ?capitalLabel WHERE { ?country wdt:P31 wd:Q6256. ?country wdt:P36 ?capital. SERVICE wikibase:label {bd:serviceParam wikibase:language "[AUTO_LANGUAGE],es".}}';
+    const apiUrl = `https://query.wikidata.org/sparql?query=${encodeURIComponent(sparqlQuery)}`;
+    const headers = { "Accept": "application/json" }
+    const respuestaWikidata = await fetch(apiUrl, {headers});
+    if (respuestaWikidata.ok) {
+    const data = await respuestaWikidata.json();
+    const numEles = data.results.bindings.length;
+    const index = Math.floor(Math.random() * numEles);
+    const result = data.results.bindings[index];
+    setpais(result.countryLabel.value);
+    setcapital(result.capitalLabel.value);
+    } else {
+      console.error("Error al realizar la consulta en Wikidata. Estado de respuesta:", respuestaWikidata.status);
     }
-  };
-
-  const handleCloseSnackbar = () => {
-    setOpenSnackbar(false);
-  };*/
+  }
 
   return (
-    <div>
-        <h1>Esta sería la pagina del juego</h1>
-    </div>
+    <Container component="main" maxWidth="xs" sx={{ marginTop: 4 }}>
+      <Typography component="h1" variant="h5" sx={{ textAlign: 'center' }}>
+        Esta sería la pagina del juego
+      </Typography>
+      <Button variant="contained" color="primary" onClick={handleShowQuestion}>
+        Genera pregunta NO FUNCIONA AUNQUE DEBERIA
+      </Button>
+      <Button variant="contained" color="primary" onClick={deberiaIrEnelServicio}>
+        Genera pregunta FUNCIONA
+      </Button>
+      {askForQuestion ? (
+        <Typography component="h1" variant="h5" sx={{ textAlign: 'center' }}>
+          Pais {pais} capital {capital}
+        </Typography>
+      ) : (
+        <Typography component="h1" variant="h5" sx={{ textAlign: 'center' }}>
+          Dale al boton
+        </Typography>
+      ) }
+    </Container>
   );
 };
 
