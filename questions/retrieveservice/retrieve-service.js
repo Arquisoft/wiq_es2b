458a4e595167a8ed044dc60a3b75efc6782f02ba
baraganio <carlossrsi07@gmail.com>
2024-03-27 17:29:02 +0100

    Conectando el servicio retrieve-service al gateway y a la aplicacion

--- questions/retrieveservice/retrieve-service.js
+++ questions/retrieveservice/retrieve-service.js
@@ -12,44 +12,9 @@ mongoose.connect(mongoUri);
 
 
 
-app.post('/retrievequestion', async (req, res) => {
-  /*const apiUrl = `https://query.wikidata.org/sparql?query=${encodeURIComponent(queries[randomQuerySelector])}&format=json`;
-
-  try {
-    // Makes the petition to the url
-    const response = await fetch(apiUrl, {
-      headers: {
-        'Accept': 'application/json'
-      }
-    });
-
-    // Check if everything was good on the petition
-    if (!response.ok) {
-      console.error('Error al realizar la consulta a Wikidata:', response.statusText);
-      return;
-    }
-
-    // Parse the response 
-    const data = await response.json();
-
-    // Send the parsed response to be selected
-    getQuestionInfo(data.results.bindings);
-
-    // Declare what will be return 
-    solution = {
-      responseQuestionObject : questionObject,
-      responseCorrectOption : correctOption,
-      responseAnswerOptions : answerOptions
-    };
-
-    saveQuestion();
-    
-    // Return the resoult with a 200 status
-    res.status(200).json(solution);
-  } catch (error) {
-    console.error('Error al realizar la consulta:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }*/
+app.post('/getquestionshistory', async (req, res) => {
+  
+  res.status(200).json('todo bien');
 });
 
 const server = app.listen(port, () => {
--- webapp/src/components/HistoricalData.js
+++ webapp/src/components/HistoricalData.js
@@ -1,11 +1,33 @@
-import { Typography} from '@mui/material';
+import axios from 'axios';
+import { Container, Typography, Button} from '@mui/material';
 
 const HistoricalData = () => {
+  const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
+
+  const  handleShowHistory = async () => {
+    try{
+      // It makes a petition to the api and store the response
+      const response = await axios.post(`${apiEndpoint}/getquestionshistory`, { });
+      console.log(response);
+    }catch (error){
+      console.error('Error:', error);
+    }    
+  }
 
   return (
-    <Typography variant="h4" gutterBottom>
+    
+    <Container component="main" maxWidth="xs" sx={{ marginTop: 4 }}>
+      <div>
+        <Typography variant="h4" gutterBottom>
           Pagina del HistoricalData
-    </Typography>
+        </Typography>
+
+        <Button variant="contained" color="primary" onClick={handleShowHistory}> 
+          Histórico de partidas
+        </Button>
+    </div>
+  </Container>
+
   );
 };
 
