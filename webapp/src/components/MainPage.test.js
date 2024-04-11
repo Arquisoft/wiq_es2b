1bea6301084bc9c5be69d65a62c3f0fc5c9f83e1
uo264915 <uo264915@uniovi.es>
2024-04-11 11:23:32 +0200

    [Test] Prueba testing MainPage

---
+++ webapp/src/components/MainPage.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import MainPage from './MainPage';
+import { UserProvider } from './UserContext';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const mockAxios = new MockAdapter(axios);
+
+describe('MainPage component', () => {
+  beforeEach(() => {
+    mockAxios.reset();
+  });
+
+  it('muestra la página principal correctamente', async () => {
+
+    render(<UserProvider>
+      <Router>
+        <MainPage />
+      </Router>
+    </UserProvider>);
+
+    const element1 = screen.getByText(/¡Bienvenido a/);
+    const element2 = screen.getByText(/WIQ 2024!/);
+    const newGameButton = screen.getByRole('button', { name: 'Nuevo juego' });
+    const historicalQuestionsButton = screen.getByRole('button', { name: 'Historial de preguntas' });
+    const historialUserDataButton = screen.getByRole('button', { name: 'Historial del usuario' });
+    const registerUsersButton = screen.getByRole('button', { name: 'Usuarios Registrados' });
+
+    // Verifica si el elemento se encuentra en el DOM
+    expect(element1).toBeInTheDocument();
+    expect(element2).toBeInTheDocument();
+
+    // Simulate user input
+    await act(async () => {
+        fireEvent.click(newGameButton);
+        fireEvent.click(historicalQuestionsButton);
+        fireEvent.click(historialUserDataButton);
+        fireEvent.click(registerUsersButton);
+      });
+  });
+});
+
+
