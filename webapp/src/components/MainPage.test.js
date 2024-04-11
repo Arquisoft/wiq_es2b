eaae4c6d7aa0ba755c7413c84d862f6aeac9c572
uo264915 <uo264915@uniovi.es>
2024-04-11 11:48:11 +0200

    Cambiod test MainPage

--- webapp/src/components/MainPage.test.js
+++ webapp/src/components/MainPage.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, screen, act } from '@testing-library/react';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import MainPage from './MainPage';
