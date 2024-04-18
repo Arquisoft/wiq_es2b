de016cc0a3463fbc9d18c5d5f967b0d1de3daf53
uo264915 <uo264915@uniovi.es>
2024-04-18 19:27:34 +0200

    Test historial de preguntas

---
+++ webapp/src/components/HistoricalData.test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import HistoricalData from './HistoricalData';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const mockAxios = new MockAdapter(axios);
+
+describe('HistoricalData component', () => {
+  beforeEach(() => {
+    mockAxios.reset();
+  });
+
+  it('muestra la página con el histórico de preguntas generadas', async () => {
+
+    mockAxios.onGet("http://localhost:8000/historicaldata").reply(200, 
+        { question: "¿Cual es la capital de Venezuela?",
+          correctOption: "Caracas",
+          incorrectOption1: "Doha",
+          incorrectOption2: "Barcelona",
+          incorrectOption3: "Nasáu"
+        });
+
+    render(<Router>
+        <HistoricalData />
+      </Router>);
+
+    const element = screen.getByText(/Historial de preguntas/);
+    const tableElement = screen.getByRole('table');
+
+    // Verifica si el elemento se encuentra en el DOM
+    expect(element).toBeInTheDocument();
+    expect(tableElement).toBeInTheDocument();
+
+
+    await waitFor(() => {
+        expect(screen.getByText('Pregunta')).toBeInTheDocument();
+        expect(screen.getByText('Opción correcta')).toBeInTheDocument();
+        expect(screen.getByText('Opción incorrecta 1')).toBeInTheDocument();
+        expect(screen.getByText('Opción incorrecta 2')).toBeInTheDocument();
+        expect(screen.getByText('Opción incorrecta 3')).toBeInTheDocument();
+
+        expect(screen.getByText('¿Cual es la capital de Venezuela?')).toBeInTheDocument();
+        expect(screen.getByText('Caracas')).toBeInTheDocument();
+        expect(screen.getByText('Doha')).toBeInTheDocument();
+        expect(screen.getByText('Barcelona')).toBeInTheDocument();
+        expect(screen.getByText('Nasáu')).toBeInTheDocument();
+      
+      });
+  });
+});
+
+
