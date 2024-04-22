0f490a827249b7dbc556aaa6064e2b5217a19839
Raymond Debasa Peralta <UO290054@uniovi.es>
2024-04-22 14:19:33 +0200

    paginacion Historico de usuario

--- webapp/src/components/HistoricalUserData.js
+++ webapp/src/components/HistoricalUserData.js
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { Container, Button } from '@mui/material';
+import { Container, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TablePagination } from '@mui/material';
+import Navbar from './Navbar';
+
 
 const HistoricalUserData = () => {
-  const navigate = useNavigate();
   const apiEndpoint = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
 
   const [gameHistory, setGameHistory] = useState([]);
   const [expandedRows, setExpandedRows] = useState([]);
-  
-  
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
   useEffect(() => {
     handleLoadHistory();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
 
   const handleLoadHistory = async () => {
     try {
@@ -31,11 +31,6 @@ const HistoricalUserData = () => {
     }
   };
 
-  const handlePreviousPage = async () => {
-    let path = '/MainPage';
-    navigate(path);
-  };
-
   const toggleRow = (index) => {
     const newExpandedRows = [...expandedRows];
     if (newExpandedRows.includes(index)) {
@@ -48,52 +43,95 @@ const HistoricalUserData = () => {
     setExpandedRows(newExpandedRows);
   };
 
+  const handleClick = (event) => {
+    const buttonText = event.target.textContent;
+    event.target.textContent = buttonText === '+' ? '-' : '+';
+  };
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+
+    return `${day}/${month}/${year} ${hours}:${minutes}`;
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const paginatedGameHistory = gameHistory.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+
   return (
-    <Container component="main" maxWidth="md" sx={{ marginTop: 4 }}>
-      <Button variant="contained" color="primary" onClick={handlePreviousPage}> 
-          Página anterior
-        </Button>
-     
-      <div>
-        <h2>Historial de Partidas:</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Fecha</th>
-              <th>Tiempo de partida (s)</th>
-              <th>Porcentaje de Aciertos</th>
-              <th>Número de Preguntas</th>
-              <th>Número de Aciertos</th>
-              <th>Número de Fallos</th>
-            </tr>
-          </thead>
-          <tbody>
-            {gameHistory.map((game, index) => (
-              <React.Fragment key={game.id}>
-                <tr onClick={() => toggleRow(index)}>
-                  <td>{game.date}</td>
-                  <td>{game.duration}</td>
-                  <td>{game.percentage}%</td>
-                  <td>{game.totalQuestions}</td>
-                  <td>{game.correctAnswers}</td>
-                  <td>{game.incorrectAnswers}</td>
-                </tr>
-                {expandedRows.includes(index) && game.questions && game.questions.map((question, qIndex) => (
-                  <tr key={qIndex}>
-                    <td colSpan="6">
-                      <p>Pregunta {qIndex + 1}: {question.question}</p>
-                      <p>Respuesta Correcta: {question.correctAnswer}</p>
-                      <p>Respuesta del Usuario: {question.userAnswer}</p>
-                      <p>La respuesta fue: {question.correctAnswer === question.userAnswer ? 'Correcta' : 'Incorrecta'}</p>
-                    </td>
-                  </tr>
+    <>
+      <Navbar />
+
+      <Typography component="h2" style={{ marginTop: '1rem' }} className='fs-2 main-title animate__animated animate__backInLeft' variant="h2" sx={{ textAlign: 'center' }}>
+          Historial de partidas de { localStorage.getItem('username')}
+      </Typography>
+
+      <Container component="main" sx={{ marginTop: 2, maxHeight: '400px', overflowX: 'auto' }} className='containerTable'>
+        <div>
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell></TableCell>
+                  <TableCell>Fecha</TableCell>
+                  <TableCell>Tiempo de partida</TableCell>
+                  <TableCell>Porcentaje de Aciertos</TableCell>
+                  <TableCell>Número de Preguntas</TableCell>
+                  <TableCell>Número de Aciertos</TableCell>
+                  <TableCell>Número de Fallos</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {paginatedGameHistory.map((game, index) => (
+                  <React.Fragment key={game.id}>
+                    <TableRow onClick={() => toggleRow(index)}>
+                      <TableCell><button onClick={handleClick} type="button" class="btn-show-more">+</button></TableCell>
+                      <TableCell className="custom-td">{formatDate(game.date)}</TableCell>
+                      <TableCell className="custom-td">{game.duration} segundos</TableCell>
+                      <TableCell className="custom-td">{game.percentage.toFixed(2)}%</TableCell>
+                      <TableCell className="custom-td">{game.totalQuestions}</TableCell>
+                      <TableCell className="custom-td">{game.correctAnswers}</TableCell>
+                      <TableCell className="custom-td">{game.incorrectAnswers}</TableCell>
+                    </TableRow>
+                    {expandedRows.includes(index) && game.questions && game.questions.map((question, qIndex) => (
+                      <TableRow key={qIndex}>
+                        <TableCell colSpan="7" style={{ backgroundColor: question.correctAnswer === question.userAnswer ? '#b8deb8' : '#daaeae' }}>
+                          <p><strong>Pregunta {qIndex + 1}:</strong> {question.question}</p>
+                          <p>Respuesta Correcta: {question.correctAnswer}</p>
+                          <p>Respuesta del Usuario: {question.userAnswer}</p>
+                          <p>La respuesta fue: {question.correctAnswer === question.userAnswer ? 'Correcta' : 'Incorrecta'}</p>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </React.Fragment>
                 ))}
-              </React.Fragment>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </Container>
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            component="div"
+            count={gameHistory.length}
+            page={page}
+            onPageChange={handleChangePage}
+            rowsPerPage={rowsPerPage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+            rowsPerPageOptions={[5, 10, 15]}
+          />
+        </div>
+      </Container>
+    </>
   );
 
 };
