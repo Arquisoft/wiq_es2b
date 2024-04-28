e8c1f17c8fd78025801a11f54c12a46c3bfd43e7
uo264915 <uo264915@uniovi.es>
2024-04-29 01:51:32 +0200

    test e2e

--- webapp/e2e/test-environment-setup.js
+++ webapp/e2e/test-environment-setup.js
@@ -5,15 +5,21 @@ let mongoserver;
 let userservice;
 let authservice;
 let gatewayservice;
+let questionservice;
+let questionhistoryservice;
 
 async function startServer() {
     console.log('Starting MongoDB memory server...');
     mongoserver = await MongoMemoryServer.create();
     const mongoUri = mongoserver.getUri();
     process.env.MONGODB_URI = mongoUri;
+    process.env.SECRET_KEY = '123456789';
+    console.log(process.env.MONGODB_URI)
     userservice = await require("../../users/userservice/user-service");
     authservice = await require("../../users/authservice/auth-service");
     gatewayservice = await require("../../gatewayservice/gateway-service");
+    questionservice = await require("../../questions/creationservice");
+    questionhistoryservice = await require("../../questions/retrieveservice");
   }
 
   startServer();
