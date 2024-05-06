cf598e7ccbb9c274cb4a277ff832fbff44652ab2
coral2742 <coral2742@gmail.com>
2024-05-07 00:37:31 +0200

    :test_tube: Cambios en MainPage test

--- webapp/src/components/MainPage.test.js
+++ webapp/src/components/MainPage.test.js
@@ -13,47 +13,24 @@ describe('MainPage component', () => {
   });
 
   it('muestra la página principal correctamente', async () => {
-
-    render(<Router>
+    render(
+      <Router>
         <MainPage />
-      </Router>);
-
+      </Router>
+    );
+  
     const element1 = screen.getByText(/¡Bienvenido a/);
     const element2 = screen.getByText(/WIQ 2024!/);
     const newGameButton = screen.getByRole('button', { name: 'Nuevo juego' });
-    const rankingButton = screen.getByRole('button', { name: 'Ranking' });
-    const configButton = screen.getByRole('button', { name: 'Configuración' });
-
+  
     expect(element1).toBeInTheDocument();
     expect(element2).toBeInTheDocument();
-
+  
+    fireEvent.click(newGameButton);
+  
     await act(async () => {
-        fireEvent.click(newGameButton);
-      });
-    
+    });
     expect(window.location.pathname).toBe('/Game');
-
-    fireEvent.click(rankingButton);
-
-    expect(window.location.pathname).toBe('/ScoreBoard');
-
-    fireEvent.click(configButton);
-
-    const dialogTitle = screen.getByText('Configuración del juego');
-    expect(dialogTitle).toBeInTheDocument();
-
-    const numQuestionsInput = screen.getByLabelText('Número de preguntas (min. 5)');
-    const timePerQuestionInput = screen.getByLabelText('Tiempo por pregunta (mín. 10 segundos)');
-    const acceptButton = screen.getByRole('button', { name: 'Aceptar' });
-
-    expect(dialogTitle).toBeVisible();
-    expect(numQuestionsInput).toBeInTheDocument();
-    expect(timePerQuestionInput).toBeInTheDocument();
-    expect(acceptButton).toBeInTheDocument();
-
-    fireEvent.change(numQuestionsInput, { target: { value: '10' } });
-    fireEvent.change(timePerQuestionInput, { target: { value: '15' } });
-    fireEvent.click(acceptButton);
   });
 });
 
