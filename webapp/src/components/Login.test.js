92d09863afe47bd20022d435b5d57ad120d6de21
coral2742 <coral2742@gmail.com>
2024-05-07 11:15:33 +0200

    :test_tube: Cambios en tests en Login de repetición de código

--- webapp/src/components/Login.test.js
+++ webapp/src/components/Login.test.js
@@ -29,7 +29,6 @@ describe('Login component', () => {
 
     const { usernameInput, passwordInput, loginButton } = getLoginElements();
 
-
     mockAxios.onPost('http://localhost:8000/login').reply(200, { createdAt: '2024-01-01T12:34:56Z' });
 
     await act(async () => {
@@ -45,7 +44,7 @@ describe('Login component', () => {
         <Login />
       </Router>);
 
-const { usernameInput, passwordInput, loginButton } = getLoginElements();
+    const { usernameInput, passwordInput, loginButton } = getLoginElements();
 
 
     // Mock del request axios.post para simular una respuesta de error
@@ -86,121 +85,5 @@ const { usernameInput, passwordInput, loginButton } = getLoginElements();
 
     expect(history.location.pathname).toBe('/');
   });
-
-
-  it('should display success message and redirect to MainPage after successful login', async () => {
-    const history = createMemoryHistory();
-    render(
-      <Router history={history}>
-        <Login />
-      </Router>
-    );
-  
-    const { usernameInput, passwordInput, loginButton } = getLoginElements();
-
-  
-    // Mock del request axios.post para simular una respuesta exitosa
-    mockAxios.onPost('http://localhost:8000/login').reply(200, { createdAt: '2024-01-01T12:34:56Z' });
-  
-    fireEvent.change(usernameInput, { target: { value: 'testUser' } });
-    fireEvent.change(passwordInput, { target: { value: 'testPassword' } });
-  
-    fireEvent.click(loginButton);
-  
-    await waitFor(() => {
-      expect(screen.getByText(/Inicio de sesión exitoso/i)).toBeInTheDocument();
-    });
-  
-    expect(history.location.pathname).toBe('/');
-  });
-
-
- 
-  
-  it('should reset error state when closing error Snackbar', async () => {
-    render(
-      <Router>
-        <Login />
-      </Router>
-    );
-  
-    const { usernameInput, passwordInput, loginButton } = getLoginElements();
-
-  
-    // Mock del request axios.post para simular una respuesta de error
-    mockAxios.onPost('http://localhost:8000/login').reply(401, { error: 'Error: Credenciales inválidas' });
-  
-    fireEvent.change(usernameInput, { target: { value: 'testUser' } });
-    fireEvent.change(passwordInput, { target: { value: 'testPassword' } });
-  
-    fireEvent.click(loginButton);
-  
-    await waitFor(() => {
-      expect(screen.getByText(/Error: Credenciales inválidas/i)).toBeInTheDocument();
-    });
-  });
-
-
-
-  it('should display success Snackbar after successful login', async () => {
-    render(
-      <Router>
-        <Login />
-      </Router>
-    );
-  
-    const { usernameInput, passwordInput, loginButton } = getLoginElements();
-
-  
-    // Mock del request axios.post para simular una respuesta exitosa
-    mockAxios.onPost('http://localhost:8000/login').reply(200, { createdAt: '2024-01-01T12:34:56Z' });
-  
-    fireEvent.change(usernameInput, { target: { value: 'testUser' } });
-    fireEvent.change(passwordInput, { target: { value: 'testPassword' } });
-  
-    fireEvent.click(loginButton);
-  
-    await waitFor(() => {
-      expect(screen.getByText(/Inicio de sesión exitoso/i)).toBeInTheDocument();
-    });
-  
-    // Verificar que el Snackbar de éxito está abierto
-    const successSnackbar = screen.getByText(/Inicio de sesión exitoso/i).closest('.MuiSnackbar-root');
-    expect(successSnackbar).toHaveClass('MuiSnackbar-root MuiSnackbar-anchorOriginBottomLeft css-cpgvjg-MuiSnackbar-root');
-  });
-  
-  it('should display success Snackbar after successful login', async () => {
-    render(
-      <Router>
-        <Login />
-      </Router>
-    );
-  
-    const { usernameInput, passwordInput, loginButton } = getLoginElements();
-
-  
-    // Mock del request axios.post para simular una respuesta exitosa
-    mockAxios.onPost('http://localhost:8000/login').reply(200, { createdAt: '2024-01-01T12:34:56Z' });
-  
-    fireEvent.change(usernameInput, { target: { value: 'testUser' } });
-    fireEvent.change(passwordInput, { target: { value: 'testPassword' } });
-  
-    fireEvent.click(loginButton);
-  
-    await waitFor(() => {
-      expect(screen.getByText(/Inicio de sesión exitoso/i)).toBeInTheDocument();
-    });
-  
-    // Verificar que el Snackbar de éxito está abierto
-    expect(screen.getByText(/Inicio de sesión exitoso/i)).toBeInTheDocument();
-  });
-
-  
-  
-  
-  
-
-  
-
 
 });
