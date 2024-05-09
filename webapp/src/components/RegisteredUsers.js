c77485b34534c530dd6b3c43b4974b8946f3d30e
coral2742 <coral2742@gmail.com>
2024-05-09 03:40:14 +0200

    🧪: Cambios en tests - registro de usuarios

--- webapp/src/components/RegisteredUsers.js
+++ webapp/src/components/RegisteredUsers.js
@@ -24,7 +24,7 @@ const RegisteredUsers = () => {
     const hours = date.getHours().toString().padStart(2, '0');
     const minutes = date.getMinutes().toString().padStart(2, '0');
 
-    return `${day}/${month}/${year} ${hours}:${minutes}`;
+    return `${day}/${month}/${year}`;
   };
 
   const handleShowHistory = async () => {
